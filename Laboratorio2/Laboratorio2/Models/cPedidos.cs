392fe5802016d0800d311901a31032350a428d84
Brayan Leal <brayanleal21@hotmail.com>
2019-03-12 18:48:14 -0600

    ALLSugarHoneyIceTea

---
+++ Laboratorio2/Laboratorio2/Models/cPedidos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio2.Models
+{
+    public class cPedidos
+    {
+        public string sNombre { get; set; }
+        public string sNit { get; set; }
+
+        public float TotalCancelar { get; set; }
+
+       
+    }
+}
